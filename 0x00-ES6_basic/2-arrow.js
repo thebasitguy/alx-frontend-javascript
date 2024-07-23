454471fe87e22594d5ab2f104f14d8a5a168a8c0
thebasitguy <mailbasitgh@gmail.com>
2024-07-23 02:37:40 +0000

    Task 2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  }
+}
