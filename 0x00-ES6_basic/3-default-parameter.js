653befb6bddf3283e3fbbce405c57eaf555fa391
thebasitguy <mailbasitgh@gmail.com>
2024-07-23 02:48:22 +0000

    Task 3

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  return initialNumber + expansion1989 + expansion2019;
+}
