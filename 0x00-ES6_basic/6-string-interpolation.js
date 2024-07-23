0b864b307f84af88bad139692dced9f57601ca35
thebasitguy <mailbasitgh@gmail.com>
2024-07-23 03:03:01 +0000

    Task 6

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
+
+  return (
+    `As of ${year}, it was the seventh-highest income county in the United States, `
+  + `with a per capita personal income of ${budget.income}. As of 2015, San Francisco `
+  + `proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`
+  );
+}
