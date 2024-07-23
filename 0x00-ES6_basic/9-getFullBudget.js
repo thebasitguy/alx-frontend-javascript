08414eb968918e71fd276b84786ee4c6d57037be
thebasitguy <mailbasitgh@gmail.com>
2024-07-23 03:13:32 +0000

    Task 9

---
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -0,0 +1,20 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+// eslint-disable-next-line import/extensions
+import getBudgetObject from './7-getBudgetObject.js';
+
+export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
+
+  return fullBudget;
+}
