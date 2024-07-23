de2b696a16a4a4f9f5c9046966b35eb00ddff23e
thebasitguy <mailbasitgh@gmail.com>
2024-07-23 02:29:24 +0000

    task 1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
