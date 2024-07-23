16639e1bbc0e9bb6a4c4b7a4f359f2f9c22a9335
thebasitguy <mailbasitgh@gmail.com>
2024-07-23 03:05:08 +0000

    Task 7

---
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
