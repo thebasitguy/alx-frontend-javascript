e75938a36186fb759acc23fa22c0b382fa0412b5
thebasitguy <mailbasitgh@gmail.com>
2024-07-23 02:58:57 +0000

    Task 5

---
+++ 0x00-ES6_basic/5-spread-operator.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable linebreak-style */
+export default function concatArrays(array1, array2, string) {
+  return [...array1, ...array2, ...string];
+}
