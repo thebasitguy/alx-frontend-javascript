019cd5f672cfe6fe0b50333ee7fc5500ea159d75
thebasitguy <mailbasitgh@gmail.com>
2024-08-01 05:06:14 +0000

    update

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -8,8 +8,8 @@ export const cpp = new Subjects.Cpp();
 export const java = new Subjects.Java();
 export const react = new Subjects.React();
 export const cTeacher: Subjects.Teacher = {
-  firstName: "Bobby",
-  lastName: "Wine",
+  firstName: "Jason",
+  lastName: "Trey",
   experienceTeachingC: 10,
 };
 
